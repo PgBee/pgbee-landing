914a79da230feb76df0d874d031bb5bd5d78d479
rubyaj24 <amaljithmvinod@gmail.com>
2025-01-01 03:29:31 +0530

    Home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Navbar from './Navbar'
+import BannerBackground from '../Assets/back.png'
 
 const Home = () => {
   return (
     <div className='home-container'>
         <Navbar/>
+        <div className='home-banner-container'>
+          <div className='home-bannerImage-container'>
+            <img src = {BannerBackground} width='400' alt=''/>
+          </div>
+          <div className='home-text-section'>
+            <h1 className='primary-heading'>
+              Accommodation in one tap
+            </h1>
+            <p className='primary-text'>
+              Find the best PGs, hostels and rooms nearby
+            </p>
+          </div>
+        </div>
     </div>
   )
 }
