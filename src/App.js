256527b93f95e83cd2fdcbcea8f668afd5d338d2
rubyaj24 <amaljithmvinod@gmail.com>
2025-01-02 14:03:13 +0530

    Landing Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import About from './Components/About';
 import Home from './Components/Home';
+import Work from './Components/Work';
+import Contacts from './Components/Contacts';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <About/>
+      <Work/>
+      <Contacts/>
+      <Footer/>
     </div>
   );
 }
