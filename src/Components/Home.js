87088139a55b89de784aec666c3bf936e9da30cb
rubyaj24 <amaljithmvinod@gmail.com>
2025-01-03 23:51:21 +0530

    Add Banner

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import BannerBackground from '../Assets/back.png'
 import { FiArrowRight } from 'react-icons/fi'
+import BannerImage from '../Assets/banner.png'
 
 const Home = () => {
   return (
@@ -10,6 +11,9 @@ const Home = () => {
         <div className='home-banner-container'>
           <div className='home-bannerImage-container'>
             <img src = {BannerBackground} width='400' alt=''/>
+          </div>
+          <div className="home-image-section">
+            <img src={BannerImage} alt="" />
           </div>
           <div className='home-text-section'>
             <h1 className='primary-heading'>
