8a05e0412cff97607403da2f17edc04a62aa943d
RugmaLS <rugma2322@gmail.com>
2025-07-29 16:36:51 +0530

    redirection whatsapp

--- src/Components/BeeSupport.js
+++ src/Components/BeeSupport.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const BeeSupport = () => {
   return (
+    <a href='https://wa.me/916235401737' target='_blank' rel='noopener noreferrer'>
     <div className='fixed-component'>BeeSupport</div>
+    </a>
   )
 }
 
