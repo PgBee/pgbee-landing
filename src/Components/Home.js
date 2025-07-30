b5265eac8510274b961b509bc09f56edf1b53e85
S SIDHARTH <sidharthpunalur@gmail.com>
2025-07-31 02:02:37 +0530

    landing animation added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import BannerBackground from '../Assets/back.png';
 import BannerImage from '../Assets/banner.png';
 import { ToastContainer, toast } from 'react-toastify';
@@ -10,29 +11,40 @@ const Home = () => {
       position: "top-right",
       autoClose: 2000,
       hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
       progress: undefined,
-      theme: "",
-      style: { backgroundColor: '#fff200', color: '#000' } 
+      style: { backgroundColor: '#fff200', color: '#000' }
     });
   };
 
   return (
     <div className='home-container'>
       <div className='home-banner-container'>
-        <div className='home-bannerImage-container'>
+        <motion.div
+          className='home-bannerImage-container'
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 2, ease: 'easeOut' }}
+        >
           <img src={BannerBackground} width='400' alt='Banner Background' />
-        </div>
+        </motion.div>
 
-        <div className="home-image-section">
+        <motion.div
+          className="home-image-section"
+          initial={{ opacity: 0, scale: 0.9 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 2, ease: 'easeOut', delay: 0.5 }}
+        >
           <img src={BannerImage} alt="Banner" />
-        </div>
+        </motion.div>
 
-        <div className='home-text-section'>
+        <motion.div
+          className='home-text-section'
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, ease: 'easeOut', delay: 1 }}
+        >
           <h1 className='primary-heading'>
-            Your Perfect Stay, Just a Click Away!
+            Your Perfect Stay, Just a Click Away!
           </h1>
           <p className='primary-text'>
             Find the best PGs, hostels and rooms nearby
@@ -53,7 +65,7 @@ const Home = () => {
               Student
             </button>
           </div>
-        </div>
+        </motion.div>
       </div>
 
       {/* Toast Notification Container */}
