5219d41352632fbb96d95dda700f38519ec47f89
S SIDHARTH <sidharthpunalur@gmail.com>
2025-07-31 01:50:19 +0530

    button toast & redirect

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,43 +1,65 @@
-import React from 'react'
-// import Navbar from './Navbar'
-import BannerBackground from '../Assets/back.png'
-import { FiArrowRight } from 'react-icons/fi'
-import BannerImage from '../Assets/banner.png'
-// import { useRef } from 'react'
+import React from 'react';
+import BannerBackground from '../Assets/back.png';
+import BannerImage from '../Assets/banner.png';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Home = () => {
+  const handleOwnerClick = () => {
+    toast('Currently only in owner mobile app !', {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "",
+      style: { backgroundColor: '#fff200', color: '#000' } 
+    });
+  };
+
   return (
     <div className='home-container'>
-        <div className='home-banner-container'>
-          <div className='home-bannerImage-container'>
-            <img src = {BannerBackground} width='400' alt=''/>
-          </div>
-          <div className="home-image-section">
-            <img src={BannerImage} alt="" />
-          </div>
-          <div className='home-text-section'>
-            <h1 className='primary-heading'>
-              Your Perfect Stay, Just a Click Away!
-            </h1>
-            <p className='primary-text'>
-              Find the best PGs, hostels and rooms nearby
-            </p>
+      <div className='home-banner-container'>
+        <div className='home-bannerImage-container'>
+          <img src={BannerBackground} width='400' alt='Banner Background' />
+        </div>
 
-            <div className='align'>
-            <a href="https://app.pgbee.in" target="_blank" rel="noopener noreferrer">
-            <button className='secondary-button'>
-               Owner's <FiArrowRight/>
+        <div className="home-image-section">
+          <img src={BannerImage} alt="Banner" />
+        </div>
+
+        <div className='home-text-section'>
+          <h1 className='primary-heading'>
+            Your Perfect Stay, Just a Click Away!
+          </h1>
+          <p className='primary-text'>
+            Find the best PGs, hostels and rooms nearby
+          </p>
+
+          <div className='align'>
+            <button 
+              className='secondary-button'
+              onClick={handleOwnerClick}
+            >
+              Owner
             </button>
-            </a>
-              <a href="https://app.pgbee.in" target="_blank" rel="noopener noreferrer">
-            <button className='secondary-button2'>
-            Student<FiArrowRight/></button>
-              </a>
-             </div>
+
+            <button 
+              onClick={() => window.location.href = "https://app.pgbee.in"} 
+              className='secondary-button2'
+            >
+              Student
+            </button>
           </div>
         </div>
-    </div>
-  )
-}
+      </div>
 
-export default Home
+      {/* Toast Notification Container */}
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Home;
