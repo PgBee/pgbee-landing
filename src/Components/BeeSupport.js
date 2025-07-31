f841380fa4837703957b1dac3d00bd51b88b8e89
Shravan Nander Pandala <97742905+Unknown-Geek@users.noreply.github.com>
2025-07-31 21:31:56 +0000

    Added whatsapp send template

--- src/Components/BeeSupport.js
+++ src/Components/BeeSupport.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const BeeSupport = () => {
+  const message = encodeURIComponent(
+    `referal code: PGB100\nname: \ndepartment;\nphone:\nhostel name:`
+  );
+  const whatsappUrl = `https://wa.me/916235401737?text=${message}`;
   return (
-    <a href='https://wa.me/916235401737' target='_blank' rel='noopener noreferrer'>
-    <div className='fixed-component'>BeeSupport</div>
-    </a>
-  )
+    <a href={whatsappUrl} target='_blank' rel='noopener noreferrer'>
+      <div className='fixed-component'>BeeSupport</div>
+    </a>
+  );
 }
 
 export default BeeSupport
