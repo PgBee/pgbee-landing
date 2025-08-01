258fd09e96afad3572a1eb666f2a4fbd17d43947
Shravan Nander Pandala <97742905+Unknown-Geek@users.noreply.github.com>
2025-08-01 09:12:38 +0000

    Shravan's fork duplicated

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className="about-section-container">
+    <div className="about-section-container" id="About">
       <div className="about-background-image-container">
         {/* <img src={AboutBackground} alt="" /> */}
       </div>
       <div className="about-section-image-container">
         {/* <img src={AboutBackgroundImage} alt="" /> */}
       </div>
-      <div className="about-section-text-container">
+      <div className="about-section-text-container" id="About">
         <p className="primary-subheading">About</p>
         <h1 className="primary-heading">
           Access to exclusive Hostels, PGs and rent without fear of misinformation
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,44 +1,39 @@
 import React, { useState } from "react";
 import Logo from "../Assets/PgBee.svg";
 // import { BsCart2 } from "react-icons/bs";
-import { HiOutlineBars3 } from "react-icons/hi2";
+import HamburgerIcon from "./HamburgerIcon";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
+// import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import HomeIcon from "@mui/icons-material/Home";
-import InfoIcon from "@mui/icons-material/Info";
-import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
-import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
-import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+// import HomeIcon from "@mui/icons-material/Home";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
+import ContactMailOutlinedIcon from "@mui/icons-material/ContactMailOutlined";
+// import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
 // import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
   const menuOptions = [
     {
-      text: "Home",
-      icon: <HomeIcon />,
-    },
-    {
       text: "About",
-      icon: <InfoIcon />,
+      icon: <InfoOutlinedIcon />,
+      href: "#About"
     },
     {
-      text: "Testimonials",
-      icon: <CommentRoundedIcon />,
+      text: "Working",
+      icon: <WorkOutlineIcon />,
+      href: "#Work"
     },
     {
       text: "Contact",
-      icon: <PhoneRoundedIcon />,
-    },
-    {
-      text: "Cart",
-      icon: <ShoppingCartRoundedIcon />,
+      icon: <ContactMailOutlinedIcon />,
+      href: "#Contact"
     },
   ];
   return (
@@ -47,33 +42,38 @@ const Navbar = () => {
         <img src={Logo} alt="" width="100"/>
       </div>
       <div className="navbar-links-container">
-        <a href="#about">About</a>
-        <a href="#contact">Contact</a>
+        <a href="#About">About</a>
+        <a href="#Work">Working</a>
+        <a href="#Contact">Contact</a>
         <a href="https://app.pgbee.in" target="_blank" rel="noopener noreferrer">
           <button className="primary-button">Search Now</button>
         </a>
       </div>
       <div className="navbar-menu-container">
-        <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+        <HamburgerIcon isOpen={openMenu} toggle={() => setOpenMenu(!openMenu)} />
       </div>
-      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right" transitionDuration={400}>
         <Box
-          sx={{ width: 250 }}
+          sx={{ width: 260, background: '#fff', height: '100%', borderTopLeftRadius: 20, borderBottomLeftRadius: 20, boxShadow: 3, p: 2 }}
           role="presentation"
           onClick={() => setOpenMenu(false)}
           onKeyDown={() => setOpenMenu(false)}
         >
           <List>
             {menuOptions.map((item) => (
-              <ListItem key={item.text} disablePadding>
-                <ListItemButton>
+              <ListItem key={item.text} disablePadding sx={{ mb: 1 }}>
+                <ListItemButton component="a" href={item.href} sx={{ borderRadius: 2, background: '#f6f6f6', mb: 1, '&:hover': { background: '#fff200' } }}>
                   <ListItemIcon>{item.icon}</ListItemIcon>
-                  <ListItemText primary={item.text} />
+                  <ListItemText primary={<span style={{fontWeight:600, fontFamily:'Red Hat Display, sans-serif'}}>{item.text}</span>} />
                 </ListItemButton>
               </ListItem>
             ))}
+            <ListItem disablePadding>
+            <ListItemButton component="a" href="https://app.pgbee.in" target="_blank" rel="noopener noreferrer" sx={{ borderRadius: 2, background: '#fff200', mt: 2 }}>
+              <ListItemText primary={<span style={{fontWeight:800, fontSize:'1.15rem', fontFamily:'Red Hat Display, sans-serif', display:'block', textAlign:'center'}}>Search Now</span>} sx={{ color: '#000' }} />
+              </ListItemButton>
+            </ListItem>
           </List>
-          <Divider />
         </Box>
       </Drawer>
     </nav>
